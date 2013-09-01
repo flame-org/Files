6d98c3d1c000dd2c6dbe0bf684604177b6314bac
JSifalda <sifalda.jiri@gmail.com>
2013-09-01 08:29:46 +0200

    Fix method joinPaths()

--- Flame/Files/Helpers/FilePathHelpers.php
+++ Flame/Files/Helpers/FilePathHelpers.php
@@ -14,40 +14,13 @@ class FilePathHelpers extends Object
 {
 
 	/**
-	 * @param $path1
-	 * @param $path2
-	 * @return bool
-	 */
-	public function useSeparator($path1, $path2)
-	{
-		if (Separator::isOnPathEnding($path1) || Separator::isOnPathBegging($path2)) {
-			return false;
-		}
-
-		return true;
-	}
-
-	/**
 	 * @return string
 	 */
 	public function joinPaths(/* $path, $path, $path... */)
 	{
-		$agrs = func_get_args();
-		$path = '';
-		if (count($agrs) > 1) {
-			$path = $agrs[0];
-			unset($agrs[0]);
-			foreach ($agrs as $arg) {
-				$arg = (string) $arg;
-				if ($this->useSeparator($path, $arg)) {
-					$path .= Separator::getSeparator() . $arg;
-				} else {
-					$path .= $arg;
-				}
-			}
-		}
-
-		return $path;
+		$separator = Separator::getSeparator();
+		$paths = array_filter(func_get_args());
+		return preg_replace('#' . $separator . '{2,}#', $separator, implode($separator, $paths));
 	}
 
 }
